b48cdb27ad9cf19885f3b89982efef8f24db33f2
Jose Gaspar <sainc2016@gmail.com>
2023-06-19 23:33:15 -0500

    url

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -5,7 +5,7 @@ const ImagePreview = ({ src }) => {
         <div className="empty-image border-info text-info">
             <img src={src} alt="" className="fluid" />
             <i className="fa-solid fa-image"></i>
-        </div>
+        </div>   
     );
 };
 
